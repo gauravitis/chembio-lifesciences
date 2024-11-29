a7c8b14f491fda7ca38fcd2f3e3c24eee67b3b83
Gaurav Sing <gauravsinghitis@gmail.com>
2024-11-29 11:00:43 +0530

    Update Next.js config for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/chembio-lifesciences' : '',
+  basePath: '/chembio-lifesciences',
 }
 
 module.exports = nextConfig
